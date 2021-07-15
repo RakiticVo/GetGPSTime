42f514d9430e2b82f2653396b200acb337ace601
= <=>
2021-07-15 14:26:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/getgpstime/GPSServices.java
@@ -0,0 +1,68 @@
+package com.example.getgpstime;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+public class GPSServices extends Service {
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationCallback locationCallback;
+
+    public GPSServices() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Log.d("mylog", "Lat is: " + locationResult.getLastLocation().getLatitude() + ", "
+                        + "Lng is: " + locationResult.getLastLocation().getLongitude());
+                Intent intent = new Intent("ACT_LOC");
+                intent.putExtra("lastLocation", locationResult.getLastLocation());
+                sendBroadcast(intent);
+            }
+        };
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        requestLocation();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    // Hàm update vị trí liên tục
+    private void requestLocation() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setSmallestDisplacement(0f);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+    }
+}
---
+++ app/src/main/java/com/example/getgpstime/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.getgpstime;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.Observer;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    // to get location permissions.
+    private final static int LOCATION_REQUEST_CODE = 23;
+    boolean locationPermission = false;
+
+    private android.location.Location location;
+    private LocationBroadcastReceiver receiver;
+    TextView tv_show_time;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        receiver = new LocationBroadcastReceiver();
+        tv_show_time = findViewById(R.id.tv_show_time);
+        // request location permission.
+        requestPermision();
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                if (!locationManager.isLocationEnabled()) {
+                    buildAlertMessageNoLocation();
+                }
+            }
+        }
+//        updateLocation();
+    }
+
+    private void updateLocation() {
+        IntentFilter filter = new IntentFilter("ACT_LOC");
+        // Đăng ký BR
+        registerReceiver(receiver, filter);
+        Toast.makeText(this, "registerReceiver success", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, GPSServices.class);
+        startService(intent);
+    }
+
+    private void requestPermision() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_REQUEST_CODE);
+        } else {
+            locationPermission = true;
+        }
+    }
+
+    private void buildAlertMessageNoLocation(){
+        new AlertDialog.Builder(this)
+                .setMessage("Your Location seems to be disabled, do you want to enable it?")
+                .setPositiveButton("Settings", new
+                        DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                            }
+                        })
+                .setNegativeButton("Cancel", null)
+                .show();
+    }
+
+    // Hàm tạo ra một BR
+    public class LocationBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.e("TAG", "onReceive:" + intent.getAction());
+            if (intent.getAction().equals("ACT_LOC")) {
+                Location mlocation = intent.getParcelableExtra("lastLocation");
+                location = mlocation;
+//                Toast.makeText(context, ""+location.getTime(), Toast.LENGTH_SHORT).show();
+//                Log.e("TAG", "onReceive: " + location.getLatitude() + "--" + location.getLongitude());
+                tv_show_time.setText(""+location.getTime());
+                
+            }
+            Toast.makeText(context, "Vị trí: " + location.getLatitude() + "--" + location.getLongitude(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopService(new Intent(MainActivity.this, GPSServices.class));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(new Intent(MainActivity.this, GPSServices.class));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateLocation();
+    }
+}
